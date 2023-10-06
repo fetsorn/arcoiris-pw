e20c35f88fc4defa70ebd3e51461326b5962ebee
fetsorn <fetsorn@gmail.com>
2023-10-06 16:12:31 +0400

    implement methods

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,7 @@ export class Module extends ModuleBase {
       ],
     }).unwrap();
 
+    // CreateGathering (index_topic_1 uint256 gatheringID, index_topic_2 address focalizer, index_topic_3 address mc, address collection, address redistribution, bool isMutable)
     const createGatheringEvent =
       "0x7278386a55533b695ad284d518708f0ebf2b7c029ca31a5c744110eab102daf9";
 
@@ -50,130 +51,103 @@ export class Module extends ModuleBase {
   }
 
   createQuiz(args: Args_createQuiz): Quiz {
-    // const txQuiz = await quizMC.createQuiz(gatheringID)
+    const tx = Ethers_Module.callContractMethodAndWait({
+      address: args.quizMC,
+      method:
+        "function createQuiz(uint256 gatheringID) external returns (uint256 quizID)",
+      args: [args.gatheringID.toString()],
+    }).unwrap();
+
+    // CreateQuiz (index_topic_1 uint256 quizID, index_topic_2 uint256 gatheringID, index_topic_3 uint256 ceremonyID, address moderator)
+    const createQuizEvent =
+      "0x67c0bcd395b0bd90ff48a3248891a5b14d7c1b5ae0be524539ffbead463f2c8d";
+
+    const index = tx.logs.findIndex(
+      (log: Ethers_Log) => log.topics[0] == createQuizEvent,
+    );
+
+    if (index == -1) {
+      throw new Error(
+        "Couldn't fetch address from event logs from transaction " +
+          tx.transactionHash,
+      );
+    }
+
+    const quizID = tx.logs[index].topics[1];
+
+    const ceremonyID = tx.logs[index].topics[3];
+
     return {
-      ceremonyID: BigInt.from(1),
-      quizID: BigInt.from(1),
+      ceremonyID: BigInt.from(ceremonyID),
+      quizID: BigInt.from(quizID),
     };
   }
 
   contribute(args: Args_contribute): Ethers_TxReceipt {
-    // .contribute(gatheringID, ceremonyID, token.target, tokenBob)
-
-    return {
-      to: "",
-      _from: "",
-      contractAddress: "",
-      gasUsed: BigInt.from(0),
-      logsBloom: "",
-      transactionHash: "",
-      logs: [],
-      blockNumber: BigInt.from(0),
-      blockHash: "",
-      cumulativeGasUsed: BigInt.from(0),
-      effectiveGasPrice: BigInt.from(0),
-    };
+    // can approve here, but cheaper to approve elsewhere with setApprovalForAll
+    return Ethers_Module.callContractMethodAndWait({
+      address: args.arcoiris,
+      method:
+        "function contribute(uint256 gatheringID, uint256 ceremonyID, address tokenAddress, uint256 tokenID)",
+      args: [
+        args.gatheringID.toString(),
+        args.ceremonyID.toString(),
+        args.token,
+        args.tokenID.toString(),
+      ],
+    }).unwrap();
   }
 
   commitCorrect(args: Args_commitCorrect): Ethers_TxReceipt {
-    // await quizMC.commitCorrect(quizID, saltHashCorrect, hashesCorrect)
-    return {
-      to: "",
-      _from: "",
-      contractAddress: "",
-      gasUsed: BigInt.from(0),
-      logsBloom: "",
-      transactionHash: "",
-      logs: [],
-      blockNumber: BigInt.from(0),
-      blockHash: "",
-      cumulativeGasUsed: BigInt.from(0),
-      effectiveGasPrice: BigInt.from(0),
-    };
+    return Ethers_Module.callContractMethodAndWait({
+      address: args.quizMC,
+      method:
+        "function commitCorrect(uint256 quizID, bytes32 saltHash, bytes32[] memory hashes) external onlyModerator(quizID)",
+      args: [args.quizID.toString(), args.saltHash, args.hashes.toString()],
+    }).unwrap();
   }
 
   commitGuess(args: Args_commitGuess): Ethers_TxReceipt {
-    // await quizMC.commitCorrect(quizID, saltHashCorrect, hashesCorrect)
-    return {
-      to: "",
-      _from: "",
-      contractAddress: "",
-      gasUsed: BigInt.from(0),
-      logsBloom: "",
-      transactionHash: "",
-      logs: [],
-      blockNumber: BigInt.from(0),
-      blockHash: "",
-      cumulativeGasUsed: BigInt.from(0),
-      effectiveGasPrice: BigInt.from(0),
-    };
+    return Ethers_Module.callContractMethodAndWait({
+      address: args.quizMC,
+      method:
+        "function commitGuess(uint256 quizID, bytes32 saltHash, bytes32[] memory hashes) external",
+      args: [args.quizID.toString(), args.saltHash, args.hashes.toString()],
+    }).unwrap();
   }
 
   endQuiz(args: Args_endQuiz): Ethers_TxReceipt {
-    // await quizMC.endQuiz(quizID)
-    return {
-      to: "",
-      _from: "",
-      contractAddress: "",
-      gasUsed: BigInt.from(0),
-      logsBloom: "",
-      transactionHash: "",
-      logs: [],
-      blockNumber: BigInt.from(0),
-      blockHash: "",
-      cumulativeGasUsed: BigInt.from(0),
-      effectiveGasPrice: BigInt.from(0),
-    };
+    return Ethers_Module.callContractMethodAndWait({
+      address: args.quizMC,
+      method: "function endQuiz(uint256 quizID) external onlyModerator(quizID)",
+      args: [args.quizID.toString()],
+    }).unwrap();
   }
 
   revealCorrect(args: Args_revealCorrect): Ethers_TxReceipt {
-    // await quizMC.revealCorrect(quizID, saltCorrect, guessesCorrect)
-    return {
-      to: "",
-      _from: "",
-      contractAddress: "",
-      gasUsed: BigInt.from(0),
-      logsBloom: "",
-      transactionHash: "",
-      logs: [],
-      blockNumber: BigInt.from(0),
-      blockHash: "",
-      cumulativeGasUsed: BigInt.from(0),
-      effectiveGasPrice: BigInt.from(0),
-    };
+    return Ethers_Module.callContractMethodAndWait({
+      address: args.quizMC,
+      method:
+        "function revealCorrect(uint256 quizID, bytes32 salt, bytes[] memory guesses) external onlyModerator(quizID)",
+      args: [args.quizID.toString(), args.salt, args.guesses.toString()],
+    }).unwrap();
   }
 
   revealGuess(args: Args_revealGuess): Ethers_TxReceipt {
-    // await quizMC.revealCorrect(quizID, saltCorrect, guessesCorrect)
-    return {
-      to: "",
-      _from: "",
-      contractAddress: "",
-      gasUsed: BigInt.from(0),
-      logsBloom: "",
-      transactionHash: "",
-      logs: [],
-      blockNumber: BigInt.from(0),
-      blockHash: "",
-      cumulativeGasUsed: BigInt.from(0),
-      effectiveGasPrice: BigInt.from(0),
-    };
+    return Ethers_Module.callContractMethodAndWait({
+      address: args.quizMC,
+      method:
+        "function revealGuess(uint256 quizID, bytes32 salt, bytes[] memory guesses) external",
+      args: [args.quizID.toString(), args.salt, args.guesses.toString()],
+    }).unwrap();
   }
 
   redistribute(args: Args_redistribute): Ethers_TxReceipt {
-    // await quizMC.redistribute(quizID)
-    return {
-      to: "",
-      _from: "",
-      contractAddress: "",
-      gasUsed: BigInt.from(0),
-      logsBloom: "",
-      transactionHash: "",
-      logs: [],
-      blockNumber: BigInt.from(0),
-      blockHash: "",
-      cumulativeGasUsed: BigInt.from(0),
-      effectiveGasPrice: BigInt.from(0),
-    };
+    return Ethers_Module.callContractMethodAndWait({
+      address: args.quizMC,
+      method:
+        "function redistribute(uint256 quizID) external onlyModerator(quizID)",
+      args: [args.quizID.toString()],
+    }).unwrap();
   }
 }
